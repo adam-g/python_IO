dd54bde8baa9c6d530b7d75943a5988ff7497367
adam-g <adam@gudjonsson.se>
2016-09-18 18:28:32 +0200

    samer

--- IR/ir_receiver.py
+++ IR/ir_receiver.py
@@ -6,25 +6,54 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.IN)
 GPIO.setup(7, GPIO.OUT)
 
-def thread_receive():
-    f = open('log','a')    
-    for x in range(300000):
-        f.write(str(read_receiver()))
-        if x % 100 == 0:
-            f.write('\n')
-            
-def thread_send():
-    for x in range(100000):
-        ir_send()
 
 def read_receiver():
-    time.sleep(0.0000069)
-    return GPIO.input(11)
+    return not GPIO.input(11)
 
+def thread_receive():
+    f = open('log','a')    
+    for i in range(3000):
+        time.sleep(0.0005)
+        p = read_receiver()
+        if p:
+            for x in range(8):
+                time.sleep(0.001)
+                f.write(str(int(read_receiver())))
+            f.write('\n')
+            time.sleep(0.001)
+    f.close()
+
+    #for x in range(300000):
+    #    f.write(str(read_receiver()))
+    #    if x % 100 == 0:
+    #        f.write('\n')
+            
 def ir_send():
     GPIO.output(7, True)
-    time.sleep(0.000021)
+    time.sleep(0.001)
     GPIO.output(7, False)
+    time.sleep(0.001)
+    GPIO.output(7, False)
+    time.sleep(0.001)
+    GPIO.output(7, False)
+    time.sleep(0.001)
+    GPIO.output(7, True)
+    time.sleep(0.001)
+    GPIO.output(7, False)
+    time.sleep(0.001)
+    GPIO.output(7, True)
+    time.sleep(0.001)
+    GPIO.output(7, False)
+    time.sleep(0.001)
+    GPIO.output(7, True)
+    time.sleep(0.001)
+    GPIO.output(7, False)
+
+def thread_send():
+    for x in range(100):
+        time.sleep(0.01)
+        ir_send()
+
 
 t_receive = threading.Thread(target = thread_receive)
 t_receive.daemon = True 
@@ -32,8 +61,14 @@ t_receive.start()
 t_send = threading.Thread(target = thread_send)
 t_send.daemon = True
 t_send.start()
-time.sleep(10)
+time.sleep(5)
 
 GPIO.cleanup()
 
+f = open('log', 'r')
+count = 0
+for line in f:  
+    if line == '11101010\n':
+        count += 1
+print count
 
